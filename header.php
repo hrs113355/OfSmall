ff087cb11ad226e980008f89c55ebe68ae193546
hrs <hrs113355@gmail.com>
2011-05-07 01:12:13 +0800

    change version

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-<h1> OfSmall - 小的 1.0 <?php echo $page_name ?> </h1>
+<h1> OfSmall - 小的 2.0 <?php echo $page_name ?> </h1>
 
 <a href="index.php">首頁</a>
 <a href="link.php">關鍵字/回應連結表</a>
