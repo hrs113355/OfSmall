0a2af12260e0f5aee58d291c2beb83115f20c86d
hrs <hrs113355@gmail.com>
2011-06-13 11:36:11 +0800

    update layout and add manage page

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-<h1> OfSmall - 小的 2.0 <?php echo $page_name ?> </h1>
+<h1> <a href="index.php">OfSmall - 小的 2.0 </a><?php echo $page_name ?> </h1>
 
 <div id="header">
 <a href="index.php">首頁</a>
 <a href="link.php">關鍵字/回應連結表</a>
-<!--<a href="reply.php">回應列表</a>
-<a href="origin.php">觸發關鍵字列表</a>-->
 <a href="http://plurk.com/ofsmall">噗浪頁面</a>
+<a href="manage.php">管理</a>
 </div>
+<hr>
 <div id="fb-root"></div><script src="http://connect.facebook.net/zh_TW/all.js#xfbml=1"></script><fb:like href="http://hrs.loli.tw/ofsmall" send="false" width="450" show_faces="true" font=""></fb:like>
 <div id="shares"> 
 <a href="javascript: void(0)" onclick="(window.open('http://www.plurk.com/?qualifier=shares&status=' .concat(encodeURIComponent(location.href)) .concat(' ') .concat('(') .concat(encodeURIComponent(document.title)) .concat(')')));"> 
@@ -19,5 +19,3 @@
 <a href="javascript: void(0)" onclick="(window.open('http://www.facebook.com/share.php?u='.concat(encodeURIComponent(location.href))));"> 
 <img src="images/facebook.png"></a> 
 </div> 
-<hr>
-
---
+++ manage.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title> OfSmall - 小的 2.0 [首頁] </title>
+    <link type="text/css" rel="stylesheet" href="style/style.css" /> 
+    </head>
+
+<body>
+<div id="main">
+<?php include 'header.php'; ?>
+
+<h2>請先登入</h2>
+<form action="manage.php" method="post">
+帳號: <input type="text" name="userid" /><br />
+密碼: <input type="password" name="passwd" /><br />
+<input type="submit" /><br />
+</form>
+
+</div>
+</body>
+</html>
